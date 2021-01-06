d0a27ea360ebfa809451794445cdb687178408ed
sriharshini023 <ssudhasureshkumar@gmail.com>
2021-01-06 16:10:45 +0530

    class35

--- sketch.js
+++ sketch.js
@@ -1,29 +1,56 @@
 var ball;
 
+var mydb;
+var myPos;
+
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    mydb = firebase.database();
+
+    myPos = mydb.ref("class35/ball");
+
+   // console.log(myPos);
+
+    myPos.on('value', readPos );  //reading of the data
+
+   
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePos(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePos(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePos(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePos(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function readPos(data){
+
+   console.log(data.val());
+
+   ball.x = data.val().x;
+    ball.y = data.val().y;
+
+}
+
+function writePos(a , b){
+
+    myPos.set({
+        'x': ball.x + a,
+        'y': ball.y + b
+
+    })
 }
